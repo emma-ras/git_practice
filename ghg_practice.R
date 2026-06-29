f3b807fbc51db7c96996bb128d70c425fa3e9099
matthewroco-calvo <matthewf.rc@gmail.com>
2026-06-29 10:55:32 -0700

    added BAU consumption practice CSV, read in data into dataframe

--- ghg_practice.R
+++ ghg_practice.R
@@ -1,2 +1,10 @@
 #################################################################
 
+library(tidyverse)
+library(here)
+library(janitor)
+
+
+BAU_consump <- read_csv(here("data","BAU_consump_practice.csv")) |> 
+  clean_names()
+  
