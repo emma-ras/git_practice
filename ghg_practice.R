7f7759b7093de21ec75b378973b47467180203b6
emma-ras <emmarasmussen@ucsb.edu>
2026-06-29 13:01:21 -0700

    add ef df to script

--- ghg_practice.R
+++ ghg_practice.R
@@ -1,1 +1,12 @@
 #################################################################
+
+library(tidyverse)
+library(here)
+library(janitor)
+
+
+ef_sector_dfp <- read_csv(here("ef_sector_data.csv")) |> 
+  clean_names()
+
+
+
